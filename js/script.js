56eb5554608a56141db486815e5e7eda222d5e48
Ivan Nikolic <i.nikolic84po@gmail.com>
2018-01-14 05:30:16 +0100

    Candidates page w/ search

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
 'use strict';
 var endpoint = "http://localhost:3333/api/";
-// var candidates = [];
-// (function (method) {
-//     var myInit = {
-//         method
-//     }
-//     var url = endpoint + "candidates";
-//     fetch(url, myInit)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             return candidates = data;
-//         })
-//     // .then(function (candidates) {
-//     //     candidates.forEach(function (candidate) {
-//     //         var el = document.createElement("h6");
-//     //         el.innerHTML = candidate.name;
-//     //         document.getElementById("app").appendChild(el);
-//     //     });
-//     // })
-// })("GET");
+var state = [];
 
 function getRequest(query, dataHandler) {
     var url = endpoint + query;
@@ -31,6 +11,7 @@ function getRequest(query, dataHandler) {
         .then(function (data) {
             console.log("response: ", data);
             dataHandler(data);
+            return state = data;
         })
         .catch(function (error) {
             console.error(error);
@@ -38,6 +19,8 @@ function getRequest(query, dataHandler) {
 }
 
 function renderCandidates(candidates) {
+    console.log(candidates);
+    document.querySelector("#app").innerHTML = "";
     var container = document.createElement("div");
     container.classList.add("container");
     var row = document.createElement("div");
@@ -49,25 +32,66 @@ function renderCandidates(candidates) {
         var output = document.createElement("div");
         output.classList.add("col", "l4", "m6", "s12");
         output.innerHTML =
-        `<div class="card large">
+            `<div class="card large" data-id="${candidate.id}">
             <div class="card-image">
                 <img src=${candidate.avatar}>
             </div>
             <div class="card-content">
             <h4>${candidate.name}</h4>
-            <a href="mailto:${candidate.email}">${candidate.email}</a>
+                <a href="mailto:${candidate.email}">${candidate.email}</a>
             </div>
         </div>`;
         document.querySelector("#app .container .row").appendChild(output);
+        attachEventListener(candidate.id);
     })
 }
 
 function getCandidates() {
     getRequest("candidates", function (data) {
         renderCandidates(data);
+        // setupEventListeners();
     });
 }
 
+function attachEventListener(id) {
+    var card = document.querySelector(`[data-id="${id}"]`);
+    card.addEventListener("click", function () {
+        var id = this.getAttribute("data-id");
+        localStorage.setItem("id", id);
+        window.location.href = "reports.html";
+    });
+}
+
+function getReports() {
+    var candidateId = parseFloat(window.localStorage.getItem("id"));
+    console.log("candidateId: ", candidateId);
+    getRequest("reports", function (data) {
+        data.forEach(function (report) {
+            console.log(report.candidateId === candidateId);
+        })
+    });
+}
+
+function setupEventListeners() {
+    var searchInput = document.getElementById("search");
+    searchInput.addEventListener("keyup", liveSearch);
+}
+
+function liveSearch(e) {
+    console.log(e.target.value);
+    var query = e.target.value.toLowerCase();
+    var arr = state.filter(function (candidate) {
+        return candidate.name.toLowerCase().includes(query);
+    });
+    renderCandidates(arr);
+}
+
 (function () {
-    getCandidates();
+    setupEventListeners();
+    if (window.location.href.indexOf("index.html") > -1) {
+        getCandidates();
+        return;
+    }
+    getReports();
+
 })();
