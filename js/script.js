7e2e6984175b33ff0fd5f87cd8ebb5ee3f1e9657
Ivan Nikolic <i.nikolic84po@gmail.com>
2018-01-14 10:05:23 +0100

    Some changez

--- js/script.js
+++ js/script.js
@@ -1,55 +1,72 @@
 'use strict';
+
 var endpoint = "http://localhost:3333/api/";
 var state = [];
 
-function getRequest(query, dataHandler) {
+function getRequest(query, dataHandler, errorHandler) {
     var url = endpoint + query;
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log("response: ", data);
             dataHandler(data);
-            return state = data;
         })
         .catch(function (error) {
-            console.error(error);
+            errorHandler(error);
         })
 }
 
+function UIerrorHandler(msg) {
+    var errorEl = document.createElement("h3");
+    errorEl.innerHTML = msg;
+    document.querySelector("#app .container .row").appendChild(errorEl);
+}
+
 function renderCandidates(candidates) {
-    console.log(candidates);
+    console.table(candidates);
     document.querySelector("#app").innerHTML = "";
+
     var container = document.createElement("div");
     container.classList.add("container");
+    document.querySelector("#app").appendChild(container);
+
     var row = document.createElement("div");
     row.classList.add("row");
-    document.querySelector("#app").appendChild(container);
     document.querySelector("#app .container").appendChild(row);
+
+    if (candidates.length <= 0) {
+        UIerrorHandler("Sorry, no matches!");
+        return;
+    }
+
     candidates.forEach(function (candidate) {
-        candidate.avatar = candidate.avatar ? candidate.avatar : './img/avatar.jpg';
+        candidate.avatar = candidate.avatar ? candidate.avatar : './img/avatar.png';
         var output = document.createElement("div");
         output.classList.add("col", "l4", "m6", "s12");
         output.innerHTML =
-            `<div class="card large" data-id="${candidate.id}">
-            <div class="card-image">
-                <img src=${candidate.avatar}>
-            </div>
+            `<div class="card medium" data-id="${candidate.id}">
+                <div class="card-image">
+                    <img src=${candidate.avatar} alt="avatar">
+                </div>
             <div class="card-content">
-            <h4>${candidate.name}</h4>
-                <a href="mailto:${candidate.email}">${candidate.email}</a>
+                <h5>${candidate.name}</h5>
+                 <a href="mailto:${candidate.email}"><i class="material-icons">email</i> ${candidate.email.toLowerCase()}</a>
             </div>
         </div>`;
         document.querySelector("#app .container .row").appendChild(output);
         attachEventListener(candidate.id);
     })
+    particles();
+
 }
 
 function getCandidates() {
     getRequest("candidates", function (data) {
-        renderCandidates(data);
-        // setupEventListeners();
+        state = JSON.parse(JSON.stringify(data));
+        renderCandidates(state);
+    }, function(){
+        UIerrorHandler("Oops! Sorry, there's been a mistake!");
     });
 }
 
@@ -66,10 +83,15 @@ function getReports() {
     var candidateId = parseFloat(window.localStorage.getItem("id"));
     console.log("candidateId: ", candidateId);
     getRequest("reports", function (data) {
+        console.log(data);
         data.forEach(function (report) {
-            console.log(report.candidateId === candidateId);
+            if (report.candidateId === candidateId) {
+                var reportEl = document.createElement("h5");
+                reportEl.innerHTML = report.candidateName;
+                document.getElementById("app").appendChild(reportEl);
+            };
         })
-    });
+    }, UIerrorHandler);
 }
 
 function setupEventListeners() {
@@ -95,3 +117,15 @@ function liveSearch(e) {
     getReports();
 
 })();
+function particles() {
+    if (!document.querySelector('#particles canvas')) {
+        particleground(document.getElementById('particles'), {
+            dotColor: '#1a237e',
+            lineColor: '#283593',
+            particleRadius: 6,
+            curveLines: true,
+            density: 9000,
+            proximity: 100
+        });
+    }
+}
